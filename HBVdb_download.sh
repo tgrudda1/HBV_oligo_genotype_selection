3fc358674269c5c70420ff9e14d7391c89a7c8cb
tgrudda1 <99344734+tgrudda1@users.noreply.github.com>
2024-06-06 15:01:29 -0400

    Create HBVdb_download.sh

---
+++ HBVdb_download.sh
@@ -0,0 +1,14 @@
+#makes a folder to deposit the sequences
+mkdir HBVdb_genomes
+#enters that folder
+cd HBVdb_genomes
+#downloads the set of sequences of each genotype into their own folder
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/A_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/B_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/C_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/D_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/E_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/F_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/G_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/H_Genomes.fas
+wget https://hbvdb.lyon.inserm.fr/data/nucleic/fasta/all_Genomes.fas
